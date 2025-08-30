f2bc304e70ab9659a2fea8385e0ad87a2d7ff7a2
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 12:39:05 +0200

    1

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> str:
-        return self.filename
+    def __enter__(self) -> "CleanUpFile":
+        return self
 
     def __exit__(
         self,
